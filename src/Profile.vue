75545b33ea7aceca80839afd1b16c42b151f4a16
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-05-14 23:18:48 +1200

    Created Profile vue

---
+++ src/Profile.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <nav class="navbar sticky-top navbar-expand navbar-dark bg-dark">
+    <router-link class="navbar-brand" :to="{name: 'Home'}">
+      <h3>SENG365 Petitions Site</h3>
+    </router-link>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'Home'}">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'Petitions'}">Petitions</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link v-if="this.getAuth()===''" class="nav-link disabled" :to="{name:'Profile'}">Profile</router-link>
+            <router-link v-else class="nav-link active" :to="{name:'Profile'}">Profile</router-link>
+          </li>
+        </ul>
+    <router-link v-if='this.getAuth()===""' tag="button" class="btn btn-outline-light ml-md-auto" :to="{name: 'Login'}"> Log In</router-link>
+    <button v-else class="btn btn-outline-light ml-md-auto" v-on:click="this.logOut">Log Out</button>
+  </nav>
+  <div class="jumbotron text-center">
+      <div class="container">
+        <h1 class="display-4">Petitions</h1>
+        <p class="lead">Browse our countless petitions for great causes below.</p>
+      </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Profile"
+    }
+</script>
