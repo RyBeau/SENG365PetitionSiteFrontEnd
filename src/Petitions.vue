e78bdbec790dcf0c3945b7eb5497ccb18e30cff7
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-05-16 20:03:14 +1200

    implemented getting the petitions, petition photos and petition categories

--- src/Petitions.vue
+++ src/Petitions.vue
@@ -30,8 +30,27 @@
         <h1 class="display-4">Petitions</h1>
         <p class="lead">Browse our countless petitions for great causes below.</p>
       </div>
+    <div v-for="petition in petitions" class="card mb-3 mx-auto text-left" style="max-width: 540px;">
+      <div class="row no-gutters">
+        <div class="col-md-4 text-center">
+          <img :src="getPetitionPhotoUrl(petition.petitionId)" class="card-img" alt="No Image Available">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{petition.title}}</h5>
+            <p class="card-text">Author: {{petition.authorName}}<br>
+              Category: {{petition.category}}<br>
+              Number of Signature: {{petition.signatureCount}}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
+
+
+</div>
+
 </template>
 
 <script>
@@ -40,8 +59,39 @@
         data(){
             return{
               error:"",
-              error_flag:false
+              error_flag:false,
+              petitions: [],
+              categories: []
             }
-        }
+          },
+      mounted() {
+          this.getPetitions();
+          this.getCategories();
+      },
+      methods: {
+          getCategories: function(){
+            this.$http.get(this.route_prefix + "petitions/categories")
+            .then((response) => {
+              this.categories = response.data;
+            }).catch((error) => {
+              this.error = "Unable to retrieve petition categories";
+              this.error_flag = true;
+            });
+          },
+          getPetitionPhotoUrl: function (id){
+            return this.route_prefix + "petitions/" + id + "/photo";
+          },
+          getPetitions: function(){
+            const query_params = "";
+            this.$http.get(this.route_prefix + "petitions?" + query_params)
+            .then((response) => {
+              this.petitions = response.data;
+            }).catch((error) => {
+              this.error = error.response.statusText;
+              this.error_flag = true;
+            });
+            console.log(this.petitions);
+          }
+      }
     }
 </script>
